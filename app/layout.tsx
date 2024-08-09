efa07938a38304ffc61d7eabcd3cf6ad269587c7
Subx1s0o <nazarsabardin@gmail.com>
2024-08-10 00:11:16 +0300

    add seo meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,42 @@ import CheckAuthHeader from "@/components/checkers/CheckAuthHeader";
 import Footer from "@/components/elements/Footer";
 import "@/styles/globals.css";
 import "modern-normalize/modern-normalize.css";
+import { Metadata } from "next";
 import "resetcss/reset.min.css";
 import ClientProviders from "./ClientProviders";
 
-export const metadata = {
-  title: "Guess The Word",
+export const metadata: Metadata = {
+  title: "Guess The Word - Fun Word-Guessing Game for All Ages",
   description:
-    "Guess the Word is a fun and engaging word-guessing game that challenges players to identify a hidden word based on clues and letter hints. The game can be played individually or in teams, and it tests players' vocabulary, spelling, and deduction skills.",
+    "Guess the Word is an exciting word-guessing game that challenges you to uncover hidden words using clever clues and hints. Perfect for all ages and skill levels!",
+  openGraph: {
+    title: "Guess The Word",
+    description: "Guess the Word is a fun and engaging word-guessing game.",
+    url: "https://guess-the-word-gtw.web.app",
+    images: [
+      new URL(
+        "/meta/image.jpg",
+        "https://guess-the-word-gtw.web.app"
+      ).toString(),
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Guess The Word",
+    description: "Guess the Word is a fun and engaging word-guessing game.",
+    images: [
+      new URL(
+        "/meta/image.jpg",
+        "https://guess-the-word-gtw.web.app"
+      ).toString(),
+    ],
+  },
+  icons: {
+    icon: new URL(
+      "/meta/favicon.svg",
+      "https://guess-the-word-gtw.web.app"
+    ).toString(),
+  },
 };
 
 export default function RootLayout({
@@ -17,7 +46,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body className="flex flex-col min-h-screen dark:bg-black text-black dark:text-white">
         <ClientProviders>
           <CheckAuthHeader />
--- components/elements/Footer.tsx
+++ components/elements/Footer.tsx
@@ -4,7 +4,12 @@ export default function Footer() {
       <div className="my-container  shadow-footer dark:shadow-none rounded-t-3xl   dark:bg-slate-900 py-7 ">
         <div className="flex justify-between mb-5 relative after:content-[''] after:absolute after:h-0.5 after:w-full after:bg-black after:-bottom-3 dark:after:bg-white  ">
           <p className="">Made in Ukraine</p>
-          <a className="" href="https://github.com/Subx1s0o" target="_blank">
+          <a
+            className=""
+            href="https://github.com/Subx1s0o"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             GitHub
           </a>
         </div>
