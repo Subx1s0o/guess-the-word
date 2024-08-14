1a38fb03bc46a2a88661172d17a7de752e82cc16
Subx1s0o <nazarsabardin@gmail.com>
2024-08-14 23:55:22 +0300

    try release client component instead server

--- app/confirm-account/[token]/page.tsx
+++ app/confirm-account/[token]/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+
+import GlobalLoader from "@/components/Loaders/GlobalLoader";
 import axios from "axios";
-import { notFound } from "next/navigation";
+import { useEffect, useState } from "react";
 import PageContent from "../PageContent";
 
 const url = process.env.NEXT_PUBLIC_BACKEND_URL;
@@ -13,26 +16,54 @@ interface IConfirmAccountPageProps {
   params: { token: string };
 }
 
-export default async function ConfirmAccountPage({
+export default function ConfirmAccountPage({
   params,
 }: IConfirmAccountPageProps) {
   const { token } = params;
-  console.log(token);
-  if (!token) {
-    return <p>here is token {token}, is undefined</p>;
-  }
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-  try {
-    const res = await axios.post(`${url}/auth/verify-oauth-token`, { token });
-    const data: IVerifyResponse = res.data;
-    console.log(res.data);
-    if (!data.valid || !data.user) {
-      notFound();
+  useEffect(() => {
+    if (!token) {
+      setError("Token is missing");
+      setLoading(false);
+      return;
     }
 
-    return <PageContent user={data.user} />;
-  } catch (error) {
-    console.log(error);
-    notFound();
+    const verifyToken = async () => {
+      try {
+        const res = await axios.post(`${url}/auth/verify-oauth-token`, {
+          token,
+        });
+        const data: IVerifyResponse = res.data;
+
+        if (data.valid && data.user) {
+          setUser(data.user);
+        } else {
+          setError("Invalid token or user not found");
+        }
+      } catch (error) {
+        setError("Failed to verify token. Please try again.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    verifyToken();
+  }, [token]);
+
+  if (loading) {
+    return <GlobalLoader />;
   }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (!user) {
+    return <div>No user found</div>;
+  }
+
+  return <PageContent user={user} />;
 }
